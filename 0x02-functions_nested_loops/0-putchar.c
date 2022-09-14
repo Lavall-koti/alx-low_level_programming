0829663211d2a09dcbfe4f928d238f9429028597
Lavall-koti <barnabasalavallkoti@yahoo.com>
2022-09-14 16:43:28 -0700

    task_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,23 +9,14 @@
  */
 int main(void)
 {
-	char ch[8] = "_putchar";
+	char text[9] = "_putchar";
 		int i = 0;
-		char c;
 
-		while (i<= 8)
-		{
-			c = ch[i];
-			_putchar(c);
-			i++;
-
-		}
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
 		_putchar('\n');
+
 		return (0);
-}
-
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
