4ba8e422b5914e3e3de02eead47f1fa7e117b8d1
Lavall-koti <barnabasalavallkoti@yahoo.com>
2022-09-14 17:32:06 -0700

    task_1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - print alphabet in lower case
+ * print_alphabet - print alphabet in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 void print_alphabet(void)
@@ -11,7 +11,7 @@ void print_alphabet(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	
+
 	{
 		_putchar(alphabet);
 	}
