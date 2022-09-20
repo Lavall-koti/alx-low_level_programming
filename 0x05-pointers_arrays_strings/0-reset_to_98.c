3b006197fc16f398126aec3bb31ecd6ad53e8ffa
Lavall-koti <barnabasalavallkoti@yahoo.com>
2022-09-20 15:04:06 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * reset_to_98 - Write a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: Number that needs to be updated
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Upon working your way through this project, you will learn about what pointers and arrays are their difference and how to use them. You will also learn about strings and how to manipulate them.  
