c55da5f936f28abe2a4763b7aa558b72d10da026
Lavall-koti <barnabasalavallkoti@yahoo.com>
2022-09-20 15:42:38 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string follow by a new line
+ * @str: pointer to the string to be printed
+ *
+ * Return: void
+ */
+
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
