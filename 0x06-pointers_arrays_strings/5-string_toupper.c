f62e3e7a8307d46bebd2ca20edd377b0de294a54
Lavall-koti <barnabasalavallkoti@yahoo.com>
2022-09-23 15:51:40 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: The string to be changed
+ *
+ * Return: uppercase
+ */
+
+char *string_toupper(char *str)
+{
+	int i, c, alpha, maxAlpha;
+
+	alpha = 97;
+	maxAlpha = alpha + 26;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		c = str[i];
+		if ((c >= alpha) && (c < maxAlpha))
+		{
+			str[i] = c - alpha + 65;
+		}
+	}
+
+	return (str);
+}
