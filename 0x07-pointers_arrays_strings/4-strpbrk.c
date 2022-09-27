004f6e193fc67409453d587d27452f69c44cabe2
Lavall-koti <barnabasalavallkoti@yahoo.com>
2022-09-27 01:16:48 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define NULL 0
 
 /**
  * _strpbrk - searches a string for any of a set of bytes.
@@ -10,20 +9,20 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int x = 0, y;
-	while (s[x] != '\0')
+int x = 0, y;
+while (s[x] != '\0')
+{
+	for (y = 0; accept[y] != '\0'; y++)
 	{
-		for (y = 0; accept[y] != '\0'; y++)
+		if (s[x] == accept[y])
 		{
-			if (s[x] == accept[y])
-			{
-				s = &s[x];
-				return (s);
-			}
-
+			s = &s[x];
+			return (s);
 		}
-		x++;
-	}
 
-	return (NULL);
+	}
+	x++;
+}
+
+return (NULL);
 }
